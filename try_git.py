f9d2cefcb071bc988df460760f336636ca94a6a9
JamesWSLim <jameslimws0514@gmail.com>
2024-01-14 16:42:50 -0500

    scraped and updated new data 2024-01-14 16:42:50.588051

--- try_git.py
+++ try_git.py
@@ -4,8 +4,8 @@ import datetime
 try:
     repo = git.Repo("/Users/jameslim/Downloads/projects/jellycat-scraping/")
     repo.git.add('-A')
-    repo.git.commit('-m', f"scraped and updated new data{datetime.datetime.now()}")
-    origin_master = repo.remote(name='origin/master')
+    repo.git.commit('-m', f"scraped and updated new data {datetime.datetime.now()}")
+    origin_master = repo.remote(name='master')
     origin_master.push()
 except:
     print("Error while pushing code!")
