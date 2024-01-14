472b614559fc5cd4aa16127492d3a8bdefb7f549
JamesWSLim <jameslimws0514@gmail.com>
2024-01-14 17:18:41 -0500

    added daily_automated_process for cronjob

--- daily_automated_process.py
+++ daily_automated_process.py
@@ -1,16 +1,27 @@
 import subprocess
 import datetime
+import git
 
 from daily_scraping import *
+subprocess.Popen('cd /Users/jameslim/Downloads/projects/jellycat-scraping/', shell=True)
+subprocess.Popen('source jellycat-env/bin/activate', shell=True)
 
 ### run docker compose
 with open("/tmp/output.log", "a") as output:
     subprocess.call("docker compose up -d", shell=True, stdout=output, stderr=output)
 
-### run daily scraping
 daily_scraping()
 
-### git commands to push delta lake data to github
-subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", ""])
+with open("/tmp/output.log", "a") as output:
+    subprocess.call("docker compose down", shell=True, stdout=output, stderr=output)
+
+### push updates to git
+try:
+    repo = git.Repo("/Users/jameslim/Downloads/projects/jellycat-scraping/")
+    repo.git.add('-A')
+    repo.git.commit('-m', f"scraped and updated new data {datetime.datetime.now()}")
+    origin_master = repo.remote(name='origin')
+    origin_master.push()
+except:
+    print("Error while pushing code!")
 
