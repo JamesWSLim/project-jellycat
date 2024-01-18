6cc8bb64a8285e7bddf40d7de9a2b32dd5c3f811
JamesWSLim <jameslimws0514@gmail.com>
2024-01-18 09:52:36 -0500

    separated git automated with daily automated

--- daily_automated_process.py
+++ daily_automated_process.py
@@ -1,6 +1,5 @@
 import subprocess
 import datetime
-import git
 import time
 import logging
 
@@ -14,12 +13,3 @@ except:
 
 time.sleep(30)
 
-### push updates to git
-try:
-    repo = git.Repo("/Users/jameslim/Downloads/projects/jellycat-scraping/")
-    repo.git.add('-A')
-    repo.git.commit('-m', f"scraped and updated new data at {datetime.datetime.now()}")
-    origin_master = repo.remote(name='origin')
-    origin_master.push()
-except:
-    print("Error while pushing code!")
