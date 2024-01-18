8432dbdfb821d526faf41df893ffd51689b634a5
JamesWSLim <jameslimws0514@gmail.com>
2024-01-18 11:00:26 -0500

    daily data on Thu Jan 18 11:00:26 EST 2024

--- git_automate.sh
+++ git_automate.sh
@@ -3,4 +3,4 @@ git add .
 DATE=$(date)
 git commit -m "daily data on $DATE"
 git push origin master
-osascript -e "display notification 'pushed to remote' with title 'SUCCESS'"
+osascript -e 'display notification "pushed to remote" with title "SUCCESS"'
